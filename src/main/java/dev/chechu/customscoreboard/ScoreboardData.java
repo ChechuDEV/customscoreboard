668e09a52eef74a5a9c9b5f4097eb18e128671a4
BuildTools <22799163+DarkDragon117@users.noreply.github.com>
2021-01-12 19:05:01 +0100

    Created an object to save scoreboard's data and clear Main class.

---
+++ src/main/java/dev/chechu/customscoreboard/ScoreboardData.java
@@ -0,0 +1,80 @@
+package dev.chechu.customscoreboard;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+
+import java.util.List;
+
+public class ScoreboardData {
+    private boolean moneyTag;
+    private boolean xyzTag;
+    private boolean membersTag;
+    private List<String> scoreboard;
+    private Economy economy;
+    private Chat chat;
+
+    public ScoreboardData(boolean moneyTag, boolean xyzTag, boolean membersTag, List<String> scoreboard, Economy economy, Chat chat) {
+        this.moneyTag = moneyTag;
+        this.xyzTag = xyzTag;
+        this.membersTag = membersTag;
+        this.scoreboard = scoreboard;
+        this.economy = economy;
+        this.chat = chat;
+    }
+
+    public boolean hasMoneyTag() {
+        return moneyTag;
+    }
+
+    public void setMoneyTag(boolean moneyTag) {
+        this.moneyTag = moneyTag;
+    }
+
+    public boolean hasXyzTag() {
+        return xyzTag;
+    }
+
+    public void setXyzTag(boolean xyzTag) {
+        this.xyzTag = xyzTag;
+    }
+
+    public boolean hasMembersTag() {
+        return membersTag;
+    }
+
+    public void setMembersTag(boolean membersTag) {
+        this.membersTag = membersTag;
+    }
+
+    public List<String> getScoreboard() {
+        return scoreboard;
+    }
+
+    public void setScoreboard(List<String> scoreboard) {
+        this.scoreboard = scoreboard;
+    }
+
+    public Economy getEconomy() {
+        return economy;
+    }
+
+    public void setEconomy(Economy economy) {
+        this.economy = economy;
+    }
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public void setChat(Chat chat) {
+        this.chat = chat;
+    }
+
+    public boolean isChatOn() {
+        return chat != null;
+    }
+
+    public boolean isEconomyOn() {
+        return economy != null;
+    }
+}
