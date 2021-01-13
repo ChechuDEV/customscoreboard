5b0a0cf8169b7f65f13dadbf44424c45a601c738
BuildTools <22799163+DarkDragon117@users.noreply.github.com>
2021-01-13 23:40:32 +0100

    Fixed perms checking, improved commands messages, fixed all commands. Help is still W.I.P.

--- src/main/java/dev/chechu/customscoreboard/events/ScoreboardCommand.java
+++ src/main/java/dev/chechu/customscoreboard/events/ScoreboardCommand.java
@@ -4,34 +4,43 @@ import dev.chechu.customscoreboard.CustomBoard;
 import dev.chechu.customscoreboard.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 public class ScoreboardCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if ( commandSender instanceof ConsoleCommandSender ) {
+            commandSender.sendMessage(ChatColor.RED + "[CustomScoreboard] Using this plugin's commands with the console may cause many problems, so please use them with players.");
+            return true;
+        }
         Player player = (Player) commandSender;
         if ( !player.hasPermission("customscoreboard")) noPerms(player);
         switch (strings[0]) {
             case "reload":
-                if ( !player.hasPermission("customscoreboard.reload")) noPerms(player);
-                reload();
+                if ( !player.hasPermission("customscoreboard.reload")) {
+                    noPerms(player);
+                    break;
+                }
+                reload(player);
                 break;
             case "hide":
-                if ( !player.hasPermission("customscoreboard.hide")) noPerms(player);
+                if ( !player.hasPermission("customscoreboard.hide")) {
+                    noPerms(player);
+                    break;
+                }
                 hide(player);
                 break;
-            case "unhide":
-                if ( !player.hasPermission("customscoreboard.hide")) noPerms(player);
-                unhide(player);
+            case "show":
+                if ( !player.hasPermission("customscoreboard.hide")) {
+                    noPerms(player);
+                    break;
+                }
+                show(player);
                 break;
             case "help":
                 help(player);
@@ -56,7 +65,7 @@ public class ScoreboardCommand implements CommandExecutor, TabCompleter {
 
         if ( commandSender.hasPermission("customscoreboard.hide")) {
             tabComplete.add("hide");
-            tabComplete.add("unhide");
+            tabComplete.add("show");
         }
 
         if ( commandSender.hasPermission("customscoreboard.reload")) tabComplete.add("reload");
@@ -65,7 +74,7 @@ public class ScoreboardCommand implements CommandExecutor, TabCompleter {
         return strings.length == 1 ? tabComplete : null;
     }
 
-    private void reload() {
+    private void reload(Player player) {
         Main.getPlugin().reloadConfig();
         Main.scoreboardData.setScoreboard(Main.getPlugin().getConfig().getStringList("scoreboard-lines"));
         Main.boards.clear();
@@ -74,34 +83,37 @@ public class ScoreboardCommand implements CommandExecutor, TabCompleter {
             if ( onlinePlayer == null ) break;
             Main.createBoard(onlinePlayer);
         }
+        player.sendMessage(ChatColor.DARK_GREEN + "[CustomScoreboard] " + ChatColor.GOLD + "Scoreboard reloaded successfully.");
     }
 
     private void hide(Player player) {
         Main.boards.stream().filter(customBoard -> customBoard.getPlayer().equals(player)).findFirst().ifPresent(CustomBoard::hideScoreboard);
+        player.sendMessage(ChatColor.DARK_GREEN + "[CustomScoreboard] " + ChatColor.GOLD + "Scoreboard " + ChatColor.RED + "hidden" + ChatColor.GOLD + ". Type " + ChatColor.GREEN + "/scoreboard show " + ChatColor.GOLD + "to show the scoreboard again.");
     }
 
-    private void unhide(Player player) {
-        Main.boards.stream().filter(customBoard -> customBoard.getPlayer().equals(player)).findFirst().ifPresent(CustomBoard::unHideScoreboard);
+    private void show(Player player) {
+        Main.boards.stream().filter(customBoard -> customBoard.getPlayer().equals(player)).findFirst().ifPresent(CustomBoard::showScoreboard);
+        player.sendMessage(ChatColor.DARK_GREEN + "[CustomScoreboard] " + ChatColor.GOLD + "Scoreboard " + ChatColor.RED + "shown" + ChatColor.GOLD + ". Type " + ChatColor.GREEN + "/scoreboard hide " + ChatColor.GOLD + "to hide the scoreboard.");
+
     }
 
     private void help(Player player) {
-        player.sendMessage(ChatColor.AQUA + "[Custom Scoreboard] HELP");
+        player.sendMessage(ChatColor.DARK_GREEN + "[CustomScoreboard] " + ChatColor.GOLD + "Showing help");
         player.sendMessage(ChatColor.AQUA + "W.I.P. :(");
     }
 
     private void info(Player player) {
-        player.sendMessage(ChatColor.AQUA + "[Custom Scoreboard] INFORMATION - V2.0-Snapshot");
-        player.sendMessage(ChatColor.AQUA + "A plugin that allows you to set Custom Scoreboards on your server.");
-        player.sendMessage(ChatColor.AQUA + "Made by Tsetsi.");
-        player.sendMessage(ChatColor.AQUA + "Check it out in Spigot: https://www.spigotmc.org/resources/custom-scoreboard.87154/");
+        player.sendMessage("");
+        player.sendMessage(ChatColor.DARK_GREEN + "[CustomScoreboard] " + ChatColor.GOLD + "Allows you to set Custom Scoreboards on your server. " + ChatColor.RED + "Made by (ME). " + ChatColor.GOLD + "Get it on " + ChatColor.BLUE + "" + ChatColor.UNDERLINE + "(my domain name is still being thought about)");
+        player.sendMessage("");
     }
 
     private void noPerms(Player player) {
-        player.sendMessage(ChatColor.RED + "[Custom Scoreboard] You have no permissions for this command.");
+        player.sendMessage(ChatColor.RED + "[CustomScoreboard] You have no permissions to use this command.");
     }
 
     private void notFound(Player player) {
-        player.sendMessage(ChatColor.RED + "[Custom Scoreboard] Command doesn't exist, please use \"" + ChatColor.AQUA + "/scoreboard help" + ChatColor.RED + "\".");
+        player.sendMessage(ChatColor.RED + "[CustomScoreboard] This command does not exist, please use " + ChatColor.AQUA + "/scoreboard help" + ChatColor.RED + ".");
     }
 
 }
