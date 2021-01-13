846252b28e0ae25100c271212984b2c5f2e26481
BuildTools <22799163+DarkDragon117@users.noreply.github.com>
2021-01-13 22:52:18 +0100

    Added a createBoard static function so onJoin, onQuit and Commands can access it. DRY

--- src/main/java/dev/chechu/customscoreboard/Main.java
+++ src/main/java/dev/chechu/customscoreboard/Main.java
@@ -1,10 +1,12 @@
 package dev.chechu.customscoreboard;
 
+import dev.chechu.customscoreboard.events.ScoreboardCommand;
 import dev.chechu.customscoreboard.events.ScoreboardListener;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,6 +15,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public class Main extends JavaPlugin {
@@ -23,6 +28,8 @@ public class Main extends JavaPlugin {
     private static Plugin plugin;
 
     public static ScoreboardData scoreboardData;
+    public static List<CustomBoard> boards = new ArrayList<>();
+
 
     @Override
     public void onEnable() {
@@ -41,8 +48,6 @@ public class Main extends JavaPlugin {
                 getPluginLoader().disablePlugin(this);
             }
         }
-
-        getConfig().getStringList("scoreboard-lines").forEach(s -> log.info(s));
 
         scoreboardData.setScoreboard(getConfig().getStringList("scoreboard-lines"));
 
@@ -74,6 +79,7 @@ public class Main extends JavaPlugin {
             else log.warning("Metrics can't be enabled");
         }
 
+        Objects.requireNonNull(getCommand("scoreboard"), "Command").setExecutor(new ScoreboardCommand());
 
         super.onEnable();
     }
@@ -111,6 +117,20 @@ public class Main extends JavaPlugin {
         scoreboardData.setChat(chat);
         return chat != null;
     }
+
+    public static void createBoard(Player player) {
+        CustomBoard board = new CustomBoard(player);
+        board.setLines(Main.scoreboardData.getScoreboard());
+        board.setTitle(Objects.requireNonNull(Main.getPlugin().getConfig().getString("scoreboard-title"), "title"));
+        if ( Main.scoreboardData.hasMoneyTag() && !Main.scoreboardData.hasXyzTag() ) {
+            board.startSchedule(100);
+        } else if ( Main.scoreboardData.hasXyzTag() ) {
+            board.startSchedule(10);
+        }
+        board.setScoreboard();
+        Main.boards.add(board);
+    }
+
 
     public static Plugin getPlugin() {
         return plugin;
